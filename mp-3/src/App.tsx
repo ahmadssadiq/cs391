a0087d7fda66b29e96f3ba07941b92b35ea9a3da
ahmadssadiq <91856553+ahmadssadiq@users.noreply.github.com>
2024-10-22 23:33:46 -0400

    completed mp-3 as

--- mp-3/src/App.tsx
+++ mp-3/src/App.tsx
@@ -13,7 +13,6 @@ import './App.css';
 function Root() {
   return (
     <>
-      <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/education" element={<Education />} />

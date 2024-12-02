c04192d6e4cdc41fb0255860d672c126927f6ab6
ahmadssadiq <91856553+ahmadssadiq@users.noreply.github.com>
2024-12-02 01:57:55 -0500

    completed vercel

--- mp-6/simple-oauth-app/pages/api/oauth/login.js
+++ mp-6/simple-oauth-app/pages/api/oauth/login.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     const clientId = process.env.GITHUB_CLIENT_ID;
-    const redirectUri = encodeURIComponent('http://localhost:3000/api/oauth/callback');
+    const redirectUri = encodeURIComponent(`${process.env.BASE_URL}/api/oauth/callback`);
     const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=read:user,user:email`;
 
     res.redirect(githubAuthUrl);
